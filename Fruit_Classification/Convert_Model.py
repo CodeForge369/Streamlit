eb24c5943fe29db87d86c28f136b25ce251b9381
Sujal <sujalstha79@gmail.com>
2026-04-24 21:43:43 +0545

    Removed unused model files and temporary scripts

--- Fruit_Classification/Convert_Model.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("Fruit_Class2.keras", compile=False)
-
-model.save("Fruit_Class2_fixed.keras")   # or Fruit_Class2_fixed.keras
-
-print("Saved successfully")
--- Fruit_Classification/TensorFlow.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model("Fruit_Class2.keras", compile=False)
-print("Loaded successfully")
